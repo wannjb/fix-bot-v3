c88ed83f0894330471b82cd9887cbb43ae20441f
Xianzhe2 <114437811+Xianzhe2@users.noreply.github.com>
2022-10-05 21:59:55 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ekadanuarta/dannstore-bot*
+let str = `*GADA AWOAKOWK*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
@@ -15,10 +15,10 @@ wa.me/6288221400832`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/itsme_danuu",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/itsme_danuu", 
-    title: 'AnyaBotV2 MultiDevice',
+    mediaUrl: "https://telegra.ph/file/9c44ff3dea63746735732.jpg",
+    mediaType: "IMAGE",
+    description: "Xian Ganteng ", 
+    title: 'Feixuan-BOT MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
