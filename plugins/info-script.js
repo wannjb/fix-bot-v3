dfd2461bf3d8c728768d0752e3717923bc1b54f3
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-07 01:06:30 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -14,10 +14,10 @@ Script bot ini dijual bila kalian mau bisa chat owner
 wa.me/62895327999292`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, str, wm,'Maa'ci','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/mhdkrnwnn_",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
+    description: "https://vt.tiktok.com/ZSR4Kw2fP/", 
     title: 'WannTrue MultiDevice',
     body: wm,
     thumbnail: thumb,
