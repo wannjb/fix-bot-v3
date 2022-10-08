dd87f755a87d41289526b04882a5bb778b55e746
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-08 10:46:38 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -6,20 +6,21 @@ let str = `*https://Instagram.com/mhdkrnwnn_*
 
 Pengen Scriptnya ?
 Ada bre, santai...
-https://github.com/ImYanXiao/Elaina-MultiDevice
+https://github.com/WH-MODS-BOT/fix-bot-v3
 
 Pengen ubah owner V2?
 Jika mau ubah owner V2 menjadi nomor owner kamu
 bisa chat owner di ⬇️
 
 wa.me/62895327999292`
-let wibu = `https://telegra.ph/file/efbf6b53a658d683aaa71.jpg` 
+let str = `*https://telegra.ph/file/efbf6b53a658d683aaa71.jpg*`
+let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Ma aci ^_^','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/mhdkrnwnn_",
     mediaType: "VIDEO",
     title: 'WannTrue-MultiDevice',
-    body: 'want source code?',
+    body: 'INSTAGRAM OFFICIAL',
     thumbnail: thumb,
     sourceUrl: sig
   }
