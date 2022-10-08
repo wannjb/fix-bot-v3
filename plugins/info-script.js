302e0c8ffa5312481346b3ecc5da4e92b7d76784
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-08 11:23:41 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -15,7 +15,7 @@ bisa chat owner di ⬇️
 wa.me/62895327999292`
 let wibu = `https://telegra.ph/file/efbf6b53a658d683aaa71.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, str, wm,'Ma aci ^_^','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm,'Ma aci ^_^','Y', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/mhdkrnwnn_",
     mediaType: "VIDEO",
     title: 'WannTrue-MultiDevice',
