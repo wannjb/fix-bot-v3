ef93acfa28d4c808154c8cab8ea66ea1cab2573e
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-08 11:02:23 +0800

    Update rpg-bonus.js

--- plugins/rpg-bonus.js
+++ plugins/rpg-bonus.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.db.data.users[m.sender].lastclaim > 86400000) {
-    conn.reply(m.chat, 'Nih Gw Kasih Modal Buat beli limit\n💰50.000 Rupiah', m)  
-    global.db.data.users[m.sender].money += 50000
+    conn.reply(m.chat, 'Nih Gw Kasih Modal Buat beli limit\n💰200.000.000 Rupiah', m)  
+    global.db.data.users[m.sender].money += 200000000
     global.db.data.users[m.sender].lastclaim = new Date * 1
-  } else conn.reply(m.chat, '📮Bagi link bokep 100.000:v', m)
+  } else conn.reply(m.chat, '📮Bagi link bokep dulu bre:v', m)
 }
 handler.help = ['bonus', 'hadiah']
 handler.tags = ['rpg']
