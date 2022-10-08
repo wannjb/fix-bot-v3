9c151d0297c62227279891e89e41dea12309951c
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-08 17:44:41 +0800

    Update download-tiktok-audio.js

--- plugins/download-tiktok-audio.js
+++ plugins/download-tiktok-audio.js
@@ -1,5 +1,4 @@
-// ---------------------------------------------------- scrape bochilteam error -----------------------------------------------------------------
-/*import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 import { toAudio } from '../lib/converter.js'
 import fetch from 'node-fetch'
 
@@ -24,51 +23,9 @@ let audio = await toAudio(f, 'mp4')
 ────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
 *Nickname:* ${nickname}
 *Description:* ${description}
-_©WH-MODS-BOT-V1🍭_
+_©WannBotz🍭_
 `.trim(), m, null, { mimetype:'audio/mpeg'})
 }
-handler.help = ['tiktokmusik', 'ttmusik', 'tiktokaudio', 'tta'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(tiktokmusik|ttmusik|tiktokaudio|tta)$/i
-
-export default handler
-*/
-// ---------------------------------------------------------------------- apikey lol human ----------------------------------------------------------
-import fetch from 'node-fetch'
-import axios from "axios"
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
- let chat = global.db.data.chats[m.chat]
-    let url = `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`
-let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    await conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`, 'tiktokaudio.mp3', `*「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ  A U D I O 」*
-    ████████▀▀▀████
-    ████████────▀██
-    ████████──█▄──█
-    ███▀▀▀██──█████
-    █▀──▄▄██──█████
-    █──█████──█████
-    █▄──▀▀▀──▄█████
-    ███▄▄▄▄▄███████
-────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
-
-*Judul:* 
-*Type:* MP3
-*📥 Ukuran File:* 
-`.trim(), m, null, {
-   document: { url: `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`}, mimetype: 'audio/mpeg', fileName: 'tiktok.mp3', conntextInfo: {
-        externalAdReply: {
-            title: '▶︎ ━━━━━━━•────────────────── ', 
-            body: 'Now Playing...',
-            description: 'Now Playing...',
-            mediaType: 2,
-          thumbnail: await (await fetch('https://telegra.ph/file/9e323ad1f4b2d52579416.jpg')).buffer(),
-         mediaUrl: sig
-        }
-     }
-  })
-}
-
 handler.help = ['tiktokmusik', 'ttmusik', 'tiktokaudio', 'tta'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktokmusik|ttmusik|tiktokaudio|tta)$/i
