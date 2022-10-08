f1deb615c72c2ec51577341792940337f2898b4d
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-08 11:48:14 +0800

    Create _creator3.js

---
+++ plugin/_creator3.js
@@ -0,0 +1,27 @@
+  let teks = 'LU KAYAK KONTOL'
+const sections = [
+   {
+	title: `${htjava} NGAMOK –––––––––·•`,
+	rows: [
+	{title: "😝 • Iyaaa Bang aku kayak kontol", rowId: "balok"},
+	{title: "🐶 • Bukan! aku ini kayak anjing", rowId: "balok"},
+	{title: "🐵 • Iyaa sih ditambah juga jelek", rowId: "balok"},
+	{title: "😱 • Iyaaa Bang kalo elu mah pasti ganteng", rowId: ".anjas"},
+	]
+    },{
+	title: `${htjava} MINTA MAAF COBA –––––––·•`,
+	rows: [
+	    {title: "💹 • Maafin aku Bang - iseng² doang", rowId: ".unas"},
+	{title: "🔖 • Maaf Bre", rowId: "unas"},
+	{title: "🌟 • Maaf kontol", rowId: "malah"},
+	]
+  },
+]
+
+const listMessage = {
+  text: teks,
+  footer: null,
+  title: `${htki} *Wann* ${htka}`,
+  buttonText: "Klik Ngentod",
+  sections
+}
