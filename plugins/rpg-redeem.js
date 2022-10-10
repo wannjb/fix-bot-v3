a07343cb46c885f79f636a6520025302e11d40df
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-11 07:43:51 +0800

    Update rpg-redeem.js

--- plugins/rpg-redeem.js
+++ plugins/rpg-redeem.js
@@ -5,14 +5,14 @@ let ctimer = (new Date - user.lastcode)
 let _ctimer = (cooldown - ctimer)
 let timers = clockString(_ctimer)
     if (args.length == 0) return conn.reply(m.chat, `Harap masukan code transaksi anda..!!`, m)
-    let kodes = args[0] == '085213' || args[0] == '443321' || args[0] == '662522' || args[0] == '322929' || args[0] == '562622' || args[0] == '432282' || args[0] == '002819' || args[0] == '715611' || args[0] == '882910' || args[0] == '882010' || args[0] == '937100' || args[0] == '736390' || args[0] == '762837' || args[0] == '028393' || args[0] == '625529' || args[0] == '727638' || args[0] == '992719' || args[0] == '092739' || args[0] == '727269' || args[0] == '629461' || args[0] == '239210'
+    let kodes = args[0] == 'WannTrue' || args[0] == '443321' || args[0] == '662522' || args[0] == '322929' || args[0] == '562622' || args[0] == '432282' || args[0] == '002819' || args[0] == '715611' || args[0] == '882910' || args[0] == '882010' || args[0] == '937100' || args[0] == '736390' || args[0] == '762837' || args[0] == '028393' || args[0] == '625529' || args[0] == '727638' || args[0] == '992719' || args[0] == '092739' || args[0] == '727269' || args[0] == '629461' || args[0] == '239210'
     if (kodes) {
    if (user.lastcode < cooldown) {
-    conn.reply(m.chat, `*SELAMAT!*\n\nKamu telah mendapatkan\n+25000 XP✨\n+250000 Money💵\n+25000 Bank🏦\n+25 Limit🌌`, m)
-    user.exp += 25000
-    user.limit += 25
+    conn.reply(m.chat, `*SELAMAT!*\n\nKamu telah mendapatkan\n+25000000000000000000000000000000000000000000000000000000000000000 XP✨\n+250000000000000000000000000000000000000000 Money💵\n+25000000000000000000000000000000000000000000000000000000000000000000000000000 Bank🏦\n+25000000000000000000000000000000000000 Limit🌌`, m)
+    user.exp += 25000000000000000000000000000000000000000000000000000000000000000000000000
+    user.limit += 25000000000000000000000000000000000000
     user.atm += 6500000
-    user.money += 250000000
+    user.money += 250000000000000000000000000000000000000000000000000000000000000000000000000000000
    user.lastcode = new Date * 1
    } else {
    return m.reply(`Hei Anda sudah mengambill code gift, Code gift anda sudah kadaluarsa..\nTunggu besok iya kak..\n${timers}`.trim())
